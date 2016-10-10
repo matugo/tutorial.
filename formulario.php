e60ee4b790316c6136309452aa3bc36934edba9a
matugo <matugoag98@gmail.com>
2016-10-10 12:34:31 -0500

    formulario.php
    Esto es un código para la creación de un formulario.

---
+++ formulario.php
@@ -0,0 +1,31 @@
+<html>
+<head>
+	<title>Formulario</title>
+</head>
+<body>
+
+<form action="archivo.php" method="get">
+<br>
+Documento
+<input type="text" name="Documento">
+<br>
+Nombres
+<input type="text" name="Nombres">
+<br>
+Apellidos
+<input type="text" name="Apellidos">
+<br>
+Fecha_Nacimiento
+<input type="date" name="Fecha_Nacimiento">
+<br>
+Celular
+<input type="text" name="Celular">
+<br>
+R.H
+<input type="text" name="R.H">
+<br>
+<input type="submit" value="Enviar">
+</form>
+
+</body>
+</html>
